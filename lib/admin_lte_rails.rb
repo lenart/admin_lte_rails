3273547ec47d79ee9bddb805103d57aee7f8732f
Lenart Rudel <lenart.rudel@gmail.com>
2015-03-16 11:21:28 +0100

    Generators

--- lib/admin_lte_rails.rb
+++ lib/admin_lte_rails.rb
@@ -1,6 +1,6 @@
-require "admin_lte_rails/version"
-require "admin_lte_rails/engine"
-require "helpers/navigation_helper"
+require 'admin_lte_rails/version'
+require 'admin_lte_rails/engine'
+require 'admin_lte_rails/navigation_helper'
 
 module AdminLteRails
 end
