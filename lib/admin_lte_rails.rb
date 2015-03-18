3ecb6ef06fad8a340111c0f5676436dd22fa780d
Lenart Rudel <lenart.rudel@gmail.com>
2015-03-18 11:37:39 +0100

    Move concern to lib

--- lib/admin_lte_rails.rb
+++ lib/admin_lte_rails.rb
@@ -1,5 +1,6 @@
 require 'admin_lte_rails/version'
 require 'admin_lte_rails/engine'
+require 'admin_lte_rails/rest_controller_concern'
 require 'admin_lte_rails/navigation_helper'
 
 module AdminLteRails
